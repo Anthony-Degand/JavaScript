1b33b022929ded12d29c87db4d140913a20d8986
Anthony <Degand.anthony@gmail.com>
2022-06-16 15:43:30 +0200

    16/06/22 - 15:43

---
+++ exo_JS_banque/script.js
@@ -0,0 +1,81 @@
+// Menu
+
+const menu = (retrait)
+
+switch (menu) {
+    case retrait:
+        console.log("Retrait");
+        break;                // Break sert a passer le switch et passer a la futur condition suivante
+    case solde:
+        console.log("Solde");
+        break;
+    case virement:
+        console.log("Virement");
+        break;
+    case dépot:
+        console.log("Dépot");
+        break;
+    case historique:
+        console.log("Historique");
+        break;
+    case rib:
+        console.log("Rib");
+        break;
+    case retour:
+        console.log("Retour");
+        break;
+
+   
+        default:                    // toujours mettre le default a la fin du switch
+        alert("erreur");
+        break;
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const menu = (5)
+
+switch (menu) {
+    case 1:
+        document.write("Retrait");
+        break;                // Break sert a passer le switch et passer a la futur condition suivante
+    case 2:
+        document.write("Solde");
+        break;
+    case 3:
+        document.write("Virement");
+        break;
+    case 4:
+        document.write("Dépot");
+        break;
+    case 5:
+        document.write("Historique");
+        break;
+    case 6:
+        document.write("Rib");
+        break;
+    case 7:
+        document.write("Retour");
+        break;
+
+   
+        default:                    // toujours mettre le default a la fin du switch
+        alert("erreur");
+        break;
+   
+}
+
+*/
---
+++ pizza_toto/script.js
@@ -0,0 +1,8 @@
+
+const chorizo = 11.50;
+const margarita = 10.00;
+const tartiflette = 12.00;
+const total = (chorizo * 1.1)+(margarita * 1.1)+(tartiflette * 1.1);
+const ht = chorizo + margarita + tartiflette;
+
+
