7f325cd953c2d267729a22df4f0f8ecc1472fc33
Anthony <Degand.anthony@gmail.com>
2022-06-17 11:07:33 +0200

    17/06/22

---
+++ exo_JS_stock/script.js
@@ -0,0 +1,14 @@
+const stk_plat = 10;
+const stk_supp = 10;
+const stk_boisson = 10;
+//stock
+
+let prix_ht = (prix_plat * qt_plat)+(prix_supp * qt_supp)+(prix_boisson * qt_boisson);
+
+
+function stock () {
+while (quantiter > stk_plat){
+    document.write("stock épuiser")
+    document.write
+}
+}
--- test_formulaire/script.js
+++ test_formulaire/script.js
@@ -1,7 +1,2 @@
 
-let carName = "Volvo";
-document.getElementById("demo").innerHTML = carName;
-
-const chorizo = 11.00.toFixed(2);
-document.getElementById("pizza").innerHTML = chorizo;
 
