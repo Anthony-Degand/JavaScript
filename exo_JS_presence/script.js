caea9086b5e1813b27b94b108fb8c09015b454b4
Anthony <Degand.anthony@gmail.com>
2022-06-21 16:05:53 +0200

    21/06/22 - 16:05

---
+++ exo_JS_presence/script.js
@@ -0,0 +1,9 @@
+let td1 = document.getElementById("td1");
+
+document.getElementById("p").onclick = function(){
+    td1.style.backgroundColor = "green";
+}
+
+document.getElementById("a").onclick = function(){
+    td1.style.backgroundColor = "red";
+}
---
+++ test/script.js
@@ -0,0 +1,8 @@
+document.getElementById("a").innerHTML = "Salutation";
+
+document.getElementById("b").onclick = function(){
+    alert("hola ! ");
+    style.backgroundColor = "red";
+}
+
+document.getElementById("h2").style.backgroundColor = "red";
