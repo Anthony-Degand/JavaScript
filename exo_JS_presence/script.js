d93532c242d3f826674dc6f22addc74205c9ac52
Anthony <Degand.anthony@gmail.com>
2022-06-22 16:06:15 +0200

    22/06/22 - 16:06

--- exo_JS_presence/script.js
+++ exo_JS_presence/script.js
@@ -1,13 +1,106 @@
 
 //jimmy
-let td1 = document.getElementById("td1");
 
-document.getElementById("p").onclick = function(){
-    td1.style.backgroundColor = "green";
+console.log(document.getElementsByName("jl"));
+
+
+
+document.getElementById("bj").onclick = function () {
+    document.getElementById("jimmy").style.backgroundColor = "green";
+}
+document.getElementById("bj2").onclick = function () {
+    document.getElementById("jimmy").style.backgroundColor = "red";
 }
 
-document.getElementById("a").onclick = function(){
-    td1.style.backgroundColor = "red";
+//cedric
+
+document.getElementById("bc").onclick = function () {
+    document.getElementById("cedric").style.backgroundColor = "green";
+}
+document.getElementById("bc2").onclick = function () {
+    document.getElementById("cedric").style.backgroundColor = "red";
 }
 
-let 
+//anthony
+
+document.getElementById("ba").onclick = function () {
+    document.getElementById("anthony").style.backgroundColor = "green";
+}
+document.getElementById("ba2").onclick = function () {
+    document.getElementById("anthony").style.backgroundColor = "red";
+}
+
+//stephane
+
+document.getElementById("bs").onclick = function () {
+    document.getElementById("stephane").style.backgroundColor = "green";
+}
+document.getElementById("bs2").onclick = function () {
+    document.getElementById("stephane").style.backgroundColor = "red";
+}
+
+//greg
+
+document.getElementById("bg").onclick = function () {
+    document.getElementById("greg").style.backgroundColor = "green";
+}
+document.getElementById("bg2").onclick = function () {
+    document.getElementById("greg").style.backgroundColor = "red";
+}
+//outman
+document.getElementById("bo").onclick = function () {
+    document.getElementById("outman").style.backgroundColor = "green";
+}
+document.getElementById("bo2").onclick = function () {
+    document.getElementById("outman").style.backgroundColor = "red";
+}
+//theo
+document.getElementById("bt").onclick = function () {
+    document.getElementById("theo").style.backgroundColor = "green";
+}
+document.getElementById("bt2").onclick = function () {
+    document.getElementById("theo").style.backgroundColor = "red";
+}
+//fred
+document.getElementById("bf").onclick = function () {
+    document.getElementById("fred").style.backgroundColor = "green";
+}
+document.getElementById("bf2").onclick = function () {
+    document.getElementById("fred").style.backgroundColor = "red";
+}
+//halim
+document.getElementById("bh").onclick = function () {
+    document.getElementById("halim").style.backgroundColor = "green";
+}
+document.getElementById("bh2").onclick = function () {
+    document.getElementById("halim").style.backgroundColor = "red";
+}
+//perrine
+document.getElementById("bp").onclick = function () {
+    document.getElementById("perrine").style.backgroundColor = "green";
+}
+document.getElementById("bp2").onclick = function () {
+    document.getElementById("perrine").style.backgroundColor = "red";
+}
+//benoit
+document.getElementById("bb").onclick = function () {
+    document.getElementById("benoit").style.backgroundColor = "green";
+}
+document.getElementById("bb2").onclick = function () {
+    document.getElementById("benoit").style.backgroundColor = "red";
+}
+//zak
+document.getElementById("bz").onclick = function () {
+    document.getElementById("zak").style.backgroundColor = "green";
+}
+document.getElementById("bz2").onclick = function () {
+    document.getElementById("zak").style.backgroundColor = "red";
+}
+//camille
+
+document.getElementById("bca").onclick = function () {
+    document.getElementById("camille").style.backgroundColor = "green";
+}
+document.getElementById("bca2").onclick = function () {
+    document.getElementById("camille").style.backgroundColor = "red";
+}
