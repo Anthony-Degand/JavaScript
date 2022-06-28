21e12de0877fbfb8a8f15b19015cf40a8f275113
Anthony <Degand.anthony@gmail.com>
2022-06-28 10:20:10 +0200

    28/06/22 - 10:20

--- pizza_toto/script.js
+++ pizza_toto/script.js
@@ -1,8 +1,16 @@
 
-const chorizo = 11.50;
+/*const chorizo = 11.50;
 const margarita = 10.00;
 const tartiflette = 12.00;
 const total = (chorizo * 1.1)+(margarita * 1.1)+(tartiflette * 1.1);
-const ht = chorizo + margarita + tartiflette;
+const ht = chorizo + margarita + tartiflette;*/
+
+function ftotal(){
+    chorizo = document.getElementById("chorizo").value;
+}
+
+function fht(){
+
+}
 
 
