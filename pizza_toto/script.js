d311c7f072da3a89be9ae84a818d2af015e77213
Anthony <Degand.anthony@gmail.com>
2022-06-28 10:51:00 +0200

    28/06/22 10:50

--- pizza_toto/script.js
+++ pizza_toto/script.js
@@ -7,6 +7,18 @@ const ht = chorizo + margarita + tartiflette;*/
 
 function ftotal(){
     chorizo = document.getElementById("chorizo").value;
+    chorizo.checked = 11.50;
+
+    margarita = document.getElementById("margarita").value;
+    chorizo.checked = 10.00;
+
+    tartiflette = document.getElementById("tartiflette").value;
+    tartiflette.checked = 12.00;
+
+    if ( chorizo.checked + margarita.checked + tartiflette.checked ) {
+        total = ( chorizo * 1.1 ) + ( margarita * 1.1 ) + ( tartiflette * 1.1 );
+    document.getElementById("clic2").innerHTML = total;
+    }
 }
 
 function fht(){
