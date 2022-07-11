47682d7384f4604fc19a8c988cbd8f8a8d03fe63
Anthony <Degand.anthony@gmail.com>
2022-07-11 11:35:41 +0200

    11/07/22 11:35

---
+++ remise_a_niv_dom_11-07-22/script.js
@@ -0,0 +1,15 @@
+
+n = document.getElementById("input").value; // je dit que "n" est égale à la valeur de mon id "input"
+
+function calcul(n){ // je crée la fonction 
+n = document.getElementById("input").value; // je lui dit que "n" est égale a la valeur de mon id "input"
+document.getElementById("result").innerHTML = `le carré de ${n} est ${n*n}`; // je lui dit que à la place de mon html id "result" j'affiche : le carré de n est égale a n * n
+}
+
+//------------------------------------------------------------------------------------------------------------
+
+function carré(nbr){
+return nbr * nbr
+}
+
+console.log(`Le carré de ${5} est égale à ${carré(5)}`);
