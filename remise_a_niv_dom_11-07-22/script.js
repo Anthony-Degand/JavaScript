5e7057fe454838df3145518cb51413b338bc4138
Anthony <Degand.anthony@gmail.com>
2022-07-25 13:02:35 +0200

    25/07/22 13:02

--- remise_a_niv_dom_11-07-22/script.js
+++ remise_a_niv_dom_11-07-22/script.js
@@ -3,7 +3,7 @@ n = document.getElementById("input").value; // je dit que "n" est égale à la v
 
 function calcul(n){ // je crée la fonction 
 n = document.getElementById("input").value; // je lui dit que "n" est égale a la valeur de mon id "input"
-document.getElementById("result").innerHTML = `le carré de ${n} est ${n*n}`; // je lui dit que à la place de mon html id "result" j'affiche : le carré de n est égale a n * n
+document.getElementById("result").innerHTML = `Le carré de ${n} est ${n*n}`; // je lui dit que à la place de mon html id "result" j'affiche : le carré de n est égale a n * n
 }
 
 //------------------------------------------------------------------------------------------------------------
@@ -12,4 +12,10 @@ function carré(nbr){
 return nbr * nbr
 }
 
+// avec backtics
 console.log(`Le carré de ${5} est égale à ${carré(5)}`);
+//sans backtics
+console.log ("le carré de " + 5 + " est égale à " + carré(5))
+
+
+//------------------------------------------------------------------------------------------------------------
