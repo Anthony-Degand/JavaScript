574c627b6b9a4b445e530342264c0c7a9c8af053
Degand Anthony <106061424+Anthony-Degand@users.noreply.github.com>
2022-07-28 19:42:30 +0200

    28/07/22 19:42

---
+++ pizza_toto2/asset/script.js
@@ -0,0 +1,6 @@
+// Créer un Panier E-commerce en JS Html Css
+//   https://www.youtube.com/watch?v=klmKSJaAyAo&ab_channel=PROJETDEVELOPPEMENT-D%C3%A9veloppeurCodeur
+
+
+// JS : Gérer un panier e-commmerce avec le localstorage
+// https://www.youtube.com/watch?v=vMT4NNFYno0&ab_channel=trainingdev
